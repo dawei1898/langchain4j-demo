2aba3110ab196f5188705edf0e207a592a617bac
dawei <187533154+dawei1898@users.noreply.github.com>
2025-09-06 17:36:22 +0800

    test: 多模态输入

---
+++ src/test/java/com/langchain4j/demo/test/chat/ChatModelTest.java
@@ -0,0 +1,88 @@
+package com.langchain4j.demo.test.chat;
+
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.SystemMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.StreamingChatModel;
+import dev.langchain4j.model.chat.request.ChatRequest;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+
+import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
+
+/**
+ * @author dawei
+ */
+
+@SpringBootTest
+@ActiveProfiles("dev")
+public class ChatModelTest {
+
+    @Resource
+    private ChatModel chatModel;
+
+
+    @Test
+    public void testChat() {
+        String message = "你谁谁啊？";
+        System.out.println("【提问】 " + message);
+
+        String content = chatModel.chat(message);
+        System.out.println("【回答】 " + content);
+    }
+
+    @Test
+    public void testChatAiMessage() {
+        String message = "你谁谁啊？";
+        System.out.println("【提问】 " + message);
+
+        UserMessage userMessage = UserMessage.from(message);
+
+        SystemMessage systemMessage = SystemMessage.from("请用中文回答。");
+
+        List<ChatMessage> chatMessages = List.of(systemMessage, userMessage);
+
+        AiMessage aiMessage = chatModel.chat(chatMessages).aiMessage();
+        String thinking = aiMessage.thinking();
+        System.out.println("thinking = " + thinking);
+        String text = aiMessage.text();
+        System.out.println("text = " + text);
+    }
+
+
+    @Test
+    public void testChatRequest() {
+        String message = "你谁谁啊？";
+        System.out.println("【提问】 " + message);
+
+        ChatRequest chatRequest = ChatRequest.builder()
+                .modelName("gpt-5-mini")
+                .messages(UserMessage.from(message))
+                .build();
+
+        ChatResponse chatResponse = chatModel.chat(chatRequest);
+
+        AiMessage aiMessage = chatResponse.aiMessage();
+        String text = aiMessage.text();
+        System.out.println("【回答】 " + text);
+        System.out.println("【id】 " + chatResponse.id());
+        System.out.println("【thinking】 " + aiMessage.thinking());
+        System.out.println("【finishReason】 " + chatResponse.finishReason());
+        System.out.println("【modelName】 " + chatResponse.modelName());
+        System.out.println("【totalTokenCount】" + chatResponse.tokenUsage().totalTokenCount());
+        System.out.println("【tokenUsage】 " + chatResponse.tokenUsage());
+        System.out.println("【metadata】 " + chatResponse.metadata());
+
+    }
+
+
+
+}
